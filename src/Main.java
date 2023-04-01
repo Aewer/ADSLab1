5375cb00d76afcc8c1af854888714cfa2d7219c9
aewer <slawa.miroshnik.2004@gmail.com>
2023-04-01 08:48:19 +0600

    Problem 1 and 2

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,48 @@
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int n;
+        int[] arr;
         System.out.println("Enter the number of problem");
         int problem = scanner.nextInt();
         switch (problem) {
             case 1:
-                findMin();
+                System.out.println("Length of array: ");
+                n = scanner.nextInt();
+                System.out.println("Enter the array");
+                arr = new int[n];
+                for (int i = 0; i < n; i++) {
+                    arr[i] = scanner.nextInt();
+                }
+                System.out.println(findMin(arr, n));
+            case 2:
+                System.out.println("Length of array: ");
+                n = scanner.nextInt();
+                System.out.println("Enter the array");
+                arr = new int[n];
+                for (int i = 0; i < n; i++) {
+                    arr[i] = scanner.nextInt();
+                }
+                System.out.println(findAverage(arr, n));
         }
     }
-    public static void findMin() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Length of array: ");
-        int n = scanner.nextInt();
-        System.out.println("Enter the array");
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
+    public static int findMin(int[] arr, int n) {
+        if (n == 1) {
+            return arr[0];
+        } else {
+            int min = findMin(arr, n-1);
+            return Math.min(min, arr[n-1]);
         }
-        int min = arr[0];
-        for (int i = 0; i < n; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            }
+    }
+    public static double findAverage(int[] arr, int n) {
+        if (n == 0) {
+            return 0;
+        } else {
+            double sum = findAverage(arr, n - 1) * (n - 1);
+            sum += arr[n - 1];
+            return sum / n;
         }
-        System.out.println("Minimum number is " + min);
     }
 }
